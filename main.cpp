1c8b497eefba2730a51a971bac007a897ec26a1f
Jasmine <jazzyysuarez@gmail.com>
2025-11-13 20:38:33 -0800

    update 1

--- main.cpp
+++ main.cpp
@@ -3,10 +3,22 @@
 
 #include "Car.h"
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 #include <deque>
+using namespace std;
+
+const int INITIAL_SIZE = 2; // initial size of deque when simulation starts
 
 int main() {
+    srand(time(0));
 
-    
+    deque<Car> tollLane;
+
+    for (int i = 0; i < INITIAL_SIZE;)
+
+    cout << "Initial queue:\n";
+    cout << "    ";
+
     return 0;
 }
